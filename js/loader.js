75f26f28938ca5c95c83352231aa4abc58023b65
Ivan Antoff <1720106+IvanAntoff@users.noreply.github.com>
2019-09-07 17:26:15 -0300

    Mejoras en el loader

--- js/loader.js
+++ js/loader.js
@@ -1,10 +1,11 @@
 var tiempo;
 
 function load() {
-    tiempo = setTimeout(showPage, 3000);
+    tiempo = setTimeout(showPage, 4500);
 }
 
 function showPage() {
     document.getElementById("loader").style.display = "none";
+    document.getElementById("loader-msg").style.display = "none";
     document.getElementById("pagina").style.display = "block";
 }
